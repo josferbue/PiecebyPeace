0df512006e290a4bdd7f40ec6c7b1993ecff23c0
xela-alex <alejandro.unisoftware@hotmail.es>
2015-03-21 22:02:53 +0100

    ADICIÓN: Nuevas clases de modelo de dominio
    Se ha añadido Volunteer, Application, Company

---
+++ app/database/migrations/2015_03_21_192254_create_companies_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompaniesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('company', function($table)
+		{
+			//Actor Abstract
+			$table->increments('id');
+			$table->string('name');
+			$table->string('email');
+			$table->boolean('banned');
+
+
+
+			$table->string('sector');
+			$table->string('description');
+			$table->string('phone');
+			$table->binary('logo');
+			$table->boolean('active');
+
+
+
+			$table->integer('user_id')->unsigned()->index();
+			$table->foreign('user_id')->references('id')->on('user');
+
+
+
+		});
+
+
+	}
+
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('company');
+	}
+
+}
