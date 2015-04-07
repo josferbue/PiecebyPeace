d25caeb8bd1b8dcdf8eaec6bd183c1caabf60a7d
xela-alex <alejandro.unisoftware@hotmail.es>
2015-04-07 02:21:12 +0200

    ADDICION: Se añadido el registro de NGO's
    Se han creado los controladores y vistas referentes al registro de NGO's
    en la aplicacion.

---
+++ app/lang/en/ngo/ngo.php
@@ -0,0 +1,26 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | User Language Lines
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+
+    'name'                    				=> 'Name',
+    'holderName'                 			=> 'Holder Name',
+    'brandName'                				=> 'Brand Name',
+    'number'                     			=> 'Number',
+    'expirationMonth'                 		=> 'Expiration Month',
+    'expirationYear'             	 		=> 'Expiration Year',
+    'cvv'               					=> 'CVV',
+    'description'               			=> 'Description',
+    'phone'               					=> 'Phone',
+    'logo'               					=> 'Logo',
+    'active'               					=> 'Active',
+
+
+);
---
+++ app/lang/es_ES/ngo/ngo.php
@@ -0,0 +1,26 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | User Language Lines
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+
+    'name'                    				=> 'Nombre',
+    'holderName'                 			=> 'Nombre del Titular',
+    'brandName'                				=> 'Nombre del Banco',
+    'number'                     			=> 'Número',
+    'expirationMonth'                 		=> 'Mes de Expiración',
+    'expirationYear'             	 		=> 'Año de Expiración',
+    'cvv'               					=> 'CVV',
+    'description'               			=> 'Descripción',
+    'phone'               					=> 'Número de Teléfono',
+    'logo'               					=> 'Logo',
+    'active'               					=> 'Activo',
+
+
+);
