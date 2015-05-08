45406e3a7ac59968ea70e66f03e0d3c25aca3a0c
José Antonio Fernández Bueno <josferbue@alum.us.es>
2015-05-08 23:49:56 +0200

    CORRECCION: imagenes de idioma

---
+++ app/lang/en/admin/charts.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jose
+ * Date: 08/05/2015
+ * Time: 23:14
+ */
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Site Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'lineTitle'	            => 'Monthly Registers',
+    'lineUnit'	            => 'Registers',
+    'lineNGO'				=> 'NGO',
+    'lineVolunteer'			=> 'Volunteer',
+    'lineCompany'			=> 'Company',
+    'pieTitle'	            => 'Total users in piece by peace',
+
+);
